4d3ef1580b93260300890bbde4673542e92c809e
Django Cass <dj.cass44@gmail.com>
2019-01-19 11:39:05 +1100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+/*
+ *    Copyright 2019 Django Cass
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.20-eap-52"
+}
+
+group = "com.django"
+version = "1.0-SNAPSHOT"
+
+apply(plugin = "java")
+
+repositories {
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("io.javalin:javalin:2.6.0")
+    implementation("org.slf4j:slf4j-simple:1.7.25")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/com/django/jmp/ui/Runner.kt
@@ -0,0 +1,8 @@
+package com.django.jmp.ui
+
+import io.javalin.Javalin
+
+fun main() {
+    val app = Javalin.create().start(7000)
+    app.get("/") { ctx -> ctx.result("Hello World") }
+}
