3572cac5fd7f033af1ec3d36f0736634496229fa
Django Cass <dj.cass44@gmail.com>
2019-01-19 14:06:15 +1100

    Added working jump endpoint

--- src/main/kotlin/com/django/jmp/api/Runner.kt
+++ src/main/kotlin/com/django/jmp/api/Runner.kt
@@ -3,13 +3,14 @@ package com.django.jmp.api
 import com.django.jmp.db.Jump
 import com.django.jmp.db.JumpJson
 import com.django.jmp.db.Jumps
+import com.django.jmp.except.EmptyPathException
 import com.django.log2.logging.Log
+import io.javalin.BadRequestResponse
 import io.javalin.Javalin
+import io.javalin.NotFoundResponse
 import io.javalin.apibuilder.ApiBuilder.*
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.StdOutSqlLogger
-import org.jetbrains.exposed.sql.addLogger
+import org.eclipse.jetty.http.HttpStatus
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.sql.Connection
@@ -43,16 +44,46 @@ fun main() {
         enableCaseSensitiveUrls()
     }.start()
     app.routes {
+        // List all items in Json format
         get("/$version/jumps") { ctx ->
             val items = arrayListOf<JumpJson>()
+            Log.i(Javalin::class.java, "API:GET -> ${ctx.path()}")
             transaction {
-                Log.i(javaClass, "API:GET -> ${ctx.path()}")
                 Log.d(javaClass, Jump.all().count().toString())
                 Jump.all().forEach {
                     items.add(JumpJson(it))
                 }
             }
             ctx.json(items)
+        }
+        // Redirect to $location (if it exists)
+        get("/$version/jump/*") { ctx ->
+            try {
+                val target = ctx.path().split("/$version/jump/")[1]
+                if(target.isBlank())
+                    throw EmptyPathException()
+                Log.d(Javalin::class.java, "Target: $target")
+                transaction {
+                    val dbtarget = Jump.find {
+                        Jumps.name.lowerCase() eq target.toLowerCase()
+                    }
+                    if(!dbtarget.empty()) {
+                        val location = dbtarget.elementAt(0).location
+                        Log.v(Javalin::class.java, "Redirecting to $location")
+                        ctx.redirect(location, HttpStatus.FOUND_302)
+                    }
+                    else
+                        throw NotFoundResponse()
+                }
+            }
+            catch (e: IndexOutOfBoundsException) {
+                Log.e(Javalin::class.java, "Invalid target: ${ctx.path()}")
+                throw BadRequestResponse()
+            }
+            catch (e: EmptyPathException) {
+                Log.e(Javalin::class.java, "Empty target")
+                throw NotFoundResponse()
+            }
         }
         put("/$version/jumpAdd") { ctx ->
             val add = ctx.bodyAsClass(Jump::class.java)
