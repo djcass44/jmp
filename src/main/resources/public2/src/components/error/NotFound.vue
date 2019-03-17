416494f45516e04ee0fb229d016688ceeec374f3
Django Cass <dj.cass44@gmail.com>
2019-03-17 17:14:54 +1100

    [FE] Updated NotFound.vue to VuetifyJS [FE] Fixed NotFound.vue not using postInit emission

--- src/main/resources/public2/src/components/error/NotFound.vue
+++ src/main/resources/public2/src/components/error/NotFound.vue
@@ -1,16 +1,10 @@
 <template>
-    <div>
-        <div class="mdl-grid">
-            <div class="mdl-layout-spacer"></div>
-            <h1 class="mdl-h1">404</h1>
-            <div class="mdl-layout-spacer"></div>
-        </div>
-        <div class="mdl-grid">
-            <div class="mdl-layout-spacer"></div>
-            <p class="mdl-h5">The page you're looking for couldn't be found.</p>
-            <div class="mdl-layout-spacer"></div>
-        </div>
-    </div>
+    <v-container grid-list-md text-xs-center>
+        <v-layout row wrap>
+            <v-flex xs12><h1 class="mdl-h1">404</h1></v-flex>
+            <v-flex xs12><p class="mdl-h5">The page you're looking for couldn't be found.</p></v-flex>
+        </v-layout>
+    </v-container>
 </template>
 <script>
 export default {
@@ -18,6 +12,9 @@ export default {
     methods: {
         setLoggedIn() {},
         authChanged() {}
+    },
+    mounted: function() {
+        this.$emit('postInit');
     }
 }
 </script>
