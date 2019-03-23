7b5c2c7a22561cf7b7180da417473781e91f9c4b
Django Cass <dj.cass44@gmail.com>
2019-03-24 09:42:42 +1100

    Switched to internal LDAP provider Added ability to verify user authentication using LDAP

--- src/main/kotlin/com/django/jmp/auth/provider/LDAPProvider.kt
+++ src/main/kotlin/com/django/jmp/auth/provider/LDAPProvider.kt
@@ -1,6 +1,22 @@
+/*
+ *    Copyright 2019 Django Cass
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.django.jmp.auth.provider
 
-import biz.stillhart.profileManagement.utils.LDAPConnection
+import com.django.jmp.auth.connect.LDAPConnection
 import com.django.jmp.db.dao.GroupData
 import com.django.jmp.db.dao.UserData
 import com.django.log2.logging.Log
@@ -19,7 +35,7 @@ class LDAPProvider: BaseProvider {
 
     override fun setup() = try {
         connection = LDAPConnection("localhost", 389, "ou=Users,dc=elastic,dc=co", "cn=admin,dc=elastic,dc=co", "password")
-        connected = connection.isConnected
+        connected = connection.connected
         Log.i(javaClass, "LDAP connected: $connected")
     }
     catch (e: AuthenticationException) {
@@ -33,7 +49,7 @@ class LDAPProvider: BaseProvider {
 
     override fun getUsers(): ArrayList<UserData> {
         val users = arrayListOf<UserData>()
-        val result = connection.getResultByCustomFilter("(objectClass=inetOrgPerson)")
+        val result = connection.searchFilter("(objectClass=inetOrgPerson)")
         for (r in result) {
             val username = r.attributes.get("uid").get(0).toString()
             val role = r.attributes.get("objectClass").get(0).toString()
@@ -48,4 +64,6 @@ class LDAPProvider: BaseProvider {
     override fun tearDown() {
         connection.close()
     }
+
+    override fun getLogin(uid: String, password: String) = connection.checkUserAuth(uid, password)
 }
--- src/test/kotlin/com/django/jmp/auth/provider/LDAPProviderTest.kt
+++ src/test/kotlin/com/django/jmp/auth/provider/LDAPProviderTest.kt
@@ -1,5 +1,22 @@
+/*
+ *    Copyright 2019 Django Cass
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.django.jmp.auth.provider
 
+import com.django.log2.logging.Log
 import org.junit.jupiter.api.Test
 
 class LDAPProviderTest {
@@ -11,5 +28,25 @@ class LDAPProviderTest {
         val users = provider.getUsers()
         assert(users.size == 5)
         provider.tearDown()
+    }
+    @Test
+    fun testLoginCorrect() {
+        val provider = LDAPProvider()
+        provider.setup()
+        assert(provider.connected)
+        val validUser = provider.getLogin("bahaaldineazarmi", "bazarmi")
+        Log.d(javaClass, "Valid user: $validUser")
+        provider.tearDown()
+        assert(validUser)
+    }
+    @Test
+    fun testLoginIncorrect() {
+        val provider = LDAPProvider()
+        provider.setup()
+        assert(provider.connected)
+        val validUser = provider.getLogin("bahaaldineazarmi", "password")
+        Log.d(javaClass, "Valid user: $validUser")
+        provider.tearDown()
+        assert(!validUser)
     }
 }
