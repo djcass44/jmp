58a9fe5ad14b50beee7f4c960c991077753c0ee5
Django Cass <dj.cass44@gmail.com>
2019-06-12 22:18:08 +1000

    Fixed Provider info only returning stats for LDAP

--- src/main/kotlin/dev/castive/jmp/api/v2_1/Props.kt
+++ src/main/kotlin/dev/castive/jmp/api/v2_1/Props.kt
@@ -17,7 +17,7 @@
 package dev.castive.jmp.api.v2_1
 
 import dev.castive.javalin_auth.auth.Providers
-import dev.castive.javalin_auth.auth.provider.LDAPProvider
+import dev.castive.javalin_auth.auth.provider.InternalProvider
 import dev.castive.jmp.Runner
 import dev.castive.jmp.api.Auth
 import dev.castive.jmp.auth.LDAPConfigBuilder
@@ -26,34 +26,39 @@ import dev.castive.jmp.db.dao.Groups
 import dev.castive.jmp.db.dao.User
 import dev.castive.jmp.db.dao.Users
 import dev.castive.jmp.except.ExceptionTracker
+import dev.castive.log2.Log
 import io.javalin.apibuilder.ApiBuilder.get
 import io.javalin.apibuilder.EndpointGroup
 import org.eclipse.jetty.http.HttpStatus
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.util.concurrent.TimeUnit
 
 class Props(private val builder: LDAPConfigBuilder, private val tracker: ExceptionTracker): EndpointGroup {
-    override fun addEndpoints() {
-        get("${Runner.BASE}/v2_1/prop/:target", { ctx ->
-            val targetProp = ctx.pathParam("target")
-            val result = when {
-                builder.properties.containsKey(targetProp) -> builder.properties.getProperty(targetProp)
-                else -> builder.properties.getOrDefault(targetProp, "undefined")
-            }
-            ctx.status(HttpStatus.OK_200).result(result.toString())
-        }, Auth.adminRoleAccess)
-        get("${Runner.BASE}/v2_1/uprop/allow_local", { ctx ->
-            ctx.status(HttpStatus.OK_200).result(builder.min.blockLocal.toString())
-        }, Auth.openAccessRole)
-        get("${Runner.BASE}/v2_1/provider/main", { ctx ->
-            val connected = Providers.primaryProvider != null && Providers.primaryProvider!!.connected()
-            val users = transaction { return@transaction User.find { Users.from eq LDAPProvider.SOURCE_NAME }.count() }
-            val groups = transaction { return@transaction Group.find { Groups.from eq LDAPProvider.SOURCE_NAME }.count() }
-            ctx.status(HttpStatus.OK_200).json(LDAPPayload(connected, users, groups))
-        }, Auth.adminRoleAccess)
-        get("${Runner.BASE}/v2_1/statistics/exception", { ctx ->
-            val res = tracker.getData()
-            ctx.status(HttpStatus.OK_200).json(res)
-        }, Auth.adminRoleAccess)
-    }
+	override fun addEndpoints() {
+		get("${Runner.BASE}/v2_1/prop/:target", { ctx ->
+			val targetProp = ctx.pathParam("target")
+			val result = when {
+				builder.properties.containsKey(targetProp) -> builder.properties.getProperty(targetProp)
+				else -> builder.properties.getOrDefault(targetProp, "undefined")
+			}
+			ctx.status(HttpStatus.OK_200).result(result.toString())
+		}, Auth.adminRoleAccess)
+		get("${Runner.BASE}/v2_1/uprop/allow_local", { ctx ->
+			ctx.status(HttpStatus.OK_200).result(builder.min.blockLocal.toString())
+		}, Auth.openAccessRole)
+		get("${Runner.BASE}/v2_1/provider/main", { ctx ->
+			val main = Providers.primaryProvider
+			val connected = main != null && main.connected()
+			val users = if(main != null) transaction { return@transaction User.find { Users.from eq main.getName() }.count() } else 0
+			val groups = if(main != null) transaction { return@transaction Group.find { Groups.from eq main.getName() }.count() } else 0
+			ctx.status(HttpStatus.OK_200).json(ProviderPayload(connected, main?.getName() ?: InternalProvider.SOURCE_NAME, users, groups))
+		}, Auth.adminRoleAccess)
+		get("${Runner.BASE}/v2_1/statistics/exception", { ctx ->
+			val time = ctx.queryParam("time", "5")?.toLongOrNull() ?: 5
+			Log.v(javaClass, "An admin is viewing all logged exceptions from the last $time minutes")
+			val res = tracker.getData(TimeUnit.MINUTES.toMillis(time))
+			ctx.status(HttpStatus.OK_200).json(res)
+		}, Auth.adminRoleAccess)
+	}
 }
-data class LDAPPayload(val connected: Boolean, val users: Int, val groups: Int)
+data class ProviderPayload(val connected: Boolean, val name: String, val users: Int, val groups: Int)
