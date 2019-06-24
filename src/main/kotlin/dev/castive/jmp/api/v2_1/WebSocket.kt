5103c89abd7f78671ad91b03193ad17cb0e4f8ad
Django Cass <dj.cass44@gmail.com>
2019-06-24 20:17:50 +1000

    Fixed Socket.IO messages not being sent to all clients when using multiple nodes (see #84)

--- src/main/kotlin/dev/castive/jmp/api/v2_1/WebSocket.kt
+++ src/main/kotlin/dev/castive/jmp/api/v2_1/WebSocket.kt
@@ -26,53 +26,53 @@ import dev.castive.log2.Log
 import io.javalin.apibuilder.EndpointGroup
 
 class WebSocket : EndpointGroup {
-    companion object {
-        const val INIT_APP = "INIT_APP"
-        const val EVENT_UPDATE = "EVENT_UPDATE"
-        const val EVENT_UPDATE_USER = "${EVENT_UPDATE}_USER"
-        const val EVENT_UPDATE_GROUP = "${EVENT_UPDATE}_GROUP"
-        const val EVENT_UPDATE_FAVICON = "${EVENT_UPDATE}_FAVICON"
-        const val EVENT_UPDATE_TITLE = "${EVENT_UPDATE}_TITLE"
+	companion object {
+		const val INIT_APP = "INIT_APP"
+		const val EVENT_UPDATE = "EVENT_UPDATE"
+		const val EVENT_UPDATE_USER = "${EVENT_UPDATE}_USER"
+		const val EVENT_UPDATE_GROUP = "${EVENT_UPDATE}_GROUP"
+		const val EVENT_UPDATE_FAVICON = "${EVENT_UPDATE}_FAVICON"
+		const val EVENT_UPDATE_TITLE = "${EVENT_UPDATE}_TITLE"
 
-        val allowSockets = Util.getEnv("SOCKET_ENABLED", "true").toBoolean()
-    }
-    private val server = SocketIOServer(Configuration().apply {
-        hostname = Util.getEnv("SOCKET_HOST", "0.0.0.0")
-        port = (Util.getEnv("SOCKET_PORT", "7001").toLongOrNull() ?: 7001).toInt()
-        socketConfig = SocketConfig().apply { isReuseAddress = true }
-        storeFactory = HazelcastStoreFactory(AuthAction.cacheLayer.hzInstance)
-    })
+		val allowSockets = Util.getEnv("SOCKET_ENABLED", "true").toBoolean()
+	}
+	private val server = SocketIOServer(Configuration().apply {
+		hostname = Util.getEnv("SOCKET_HOST", "0.0.0.0")
+		port = (Util.getEnv("SOCKET_PORT", "7001").toLongOrNull() ?: 7001).toInt()
+		socketConfig = SocketConfig().apply { isReuseAddress = true }
+		storeFactory = HazelcastStoreFactory(AuthAction.cacheLayer.hzInstance)
+	})
 
-    /**
-     * Start the Socket.IO server
-     * addEndpoints is just to match the Javalin convention
-     */
-    override fun addEndpoints() {
-        if(!allowSockets) {
-            Log.i(javaClass, "Not starting Socket.IO server as env has disabled it")
-            return
-        }
-        server.apply {
-            addConnectListener {
-                Log.d(javaClass, "WebSocket connected: ${it.remoteAddress}")
-                it.sendEvent(INIT_APP, AuthAction.getAppId())
-            }
-            addDisconnectListener {
-                Log.d(javaClass, "WebSocket disconnected: ${it.remoteAddress}")
-            }
-        }.startAsync()
-    }
+	/**
+	 * Start the Socket.IO server
+	 * addEndpoints is just to match the Javalin convention
+	 */
+	override fun addEndpoints() {
+		if(!allowSockets) {
+			Log.i(javaClass, "Not starting Socket.IO server as env has disabled it")
+			return
+		}
+		server.apply {
+			addConnectListener {
+				Log.d(javaClass, "WebSocket connected: ${it.remoteAddress}")
+				it.sendEvent(INIT_APP, AuthAction.getAppId())
+			}
+			addDisconnectListener {
+				Log.d(javaClass, "WebSocket disconnected: ${it.remoteAddress}")
+			}
+		}.startAsync()
+	}
 
-    /**
-     * Broadcasts a message to all listening sessions
-     */
-    fun fire(tag: String, data: Any) {
-        if(!allowSockets) {
-            Log.i(javaClass, "Unable to broadcast as Sockets are disabled")
-            return
-        }
-        Log.v(javaClass, "Broadcasting $tag event to ${server.allClients.size} listeners")
-        server.allClients.forEach { it.sendEvent(tag, data) }
-    }
+	/**
+	 * Broadcasts a message to all listening sessions
+	 */
+	fun fire(tag: String, data: Any) {
+		if(!allowSockets) {
+			Log.i(javaClass, "Unable to broadcast as Sockets are disabled")
+			return
+		}
+		Log.v(javaClass, "Broadcasting $tag event to ${server.broadcastOperations.clients.size} listeners")
+		server.broadcastOperations.clients.forEach { it.sendEvent(tag, data) }
+	}
 }
 data class FaviconPayload(val id: Int, val url: String)
